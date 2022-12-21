26500fe237af393b45c599b6d6e93131a6b3e80b
Tony Mai <tonay6@gmail.com>
2022-12-20 21:26:10 -0600

    engineer prompts

--- index.js
+++ index.js
@@ -272,3 +272,115 @@ inquirer
                 }
             })
     }
+
+    function addEngineer() {
+        inquirer
+            //Create an engineer
+            .prompt([
+                {
+                    message: "Engineer name:",
+                    type: "input",
+                    name: "engName",
+                    validate: input => {
+                        if (input) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter a name")
+                        }
+                    }
+                },
+                {
+                    message: "Engineer ID:",
+                    type: "input",
+                    name: "engId",
+                    validate: input => {
+                        if (input) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter an ID")
+                        }
+                    }
+                },
+                {
+                    message: "Engineer email:",
+                    type: "input",
+                    name: "engEmail",
+                    validate: input => {
+                        if (input) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter an email")
+                        }
+                    }
+                },
+                {
+                    message: "Engineer GitHub username:",
+                    type: "input",
+                    name: "engGH",
+                    validate: input => {
+                        if (input) {
+                            return true;
+                        }
+                        else {
+                            console.log("Please enter a GitHub username")
+                        }
+                    }
+                },
+                {
+                    message: 'Choose next employee below:',
+                    type: 'list',
+                    name: 'add',
+                    choices: [
+                        "Intern",
+                        "Engineer",
+                        "I am finished",
+                    ]
+                }])
+    
+            //Push response data to outputAns array
+            .then((r) => {
+                outputAns.push(r)
+                //Create a new instance of Engineer
+                var newEng = new Engineer(r.engName, r.engId, r.engEmail, r.engGH);
+    
+                //Create new employee card with Intern data
+                let engName = newEng.getName();
+                let engID = newEng.getId();
+                let engEmail = newEng.getEmail();
+                let engGH = newEng.getGh();
+                let role = newEng.getRole();
+    
+                var card =
+                    `<!--Card-->
+                <article class="container shadow-md bg-blue-100 rounded-lg text-center">
+                <div class="bg-blue-900 rounded-t-lg py-2 capitalize">
+                <h1 class="text-3xl text-white font-semibold">${engName}</h1>
+                <h2 class="text-lg text-slate-300">ID: ${engID}</h2>
+                </div>
+                <div class="ml-3 m-2 text-xl font-semibold">${role}</div>
+                <div class="ml-3 mb-2 hover:text-blue-700 hover:font-semibold"><a href = "mailto: ${engEmail}">${engEmail}</a></div>
+                <div class="ml-3 mb-3 hover:text-blue-700 hover:font-semibold"><a href = "https://github.com/${engGH}">GitHub</a></div>
+                </article>`
+    
+                //Add new card to htmlCards string
+                htmlCards += card
+    
+                //If user selects to add an intern, run addIntern function
+                if (r.add == "Intern") {
+                    addIntern()
+                }
+                //If user selects to add an engineer, run addEngineer function
+                else if (r.add == "Engineer") {
+                    addEngineer();
+                }
+                //If user selects 'I am finished', run html function and create html document
+                else {
+                    html();
+                }
+            })
+    }
+    
+    
